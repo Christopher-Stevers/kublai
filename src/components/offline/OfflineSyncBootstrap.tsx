4b4ea8663d600e967ebfd935360d48a9091283f5
Halvor <halvor@local>
2026-05-10 21:46:40 -0400

    Reset sync debug timeline on app start

--- src/components/offline/OfflineSyncBootstrap.tsx
+++ src/components/offline/OfflineSyncBootstrap.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
 import { useOfflineMaterialListSync } from "~/hooks/use-offline-material-list-sync";
+import { clearSyncDebugTimeline } from "~/lib/offline-sync-debug-timeline";
 
 export function OfflineSyncBootstrap() {
+  useEffect(() => {
+    clearSyncDebugTimeline();
+  }, []);
+
   useOfflineMaterialListSync();
   return null;
 }
